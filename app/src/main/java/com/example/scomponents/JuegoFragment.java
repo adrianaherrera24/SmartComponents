cd13643a799e5bc001aa2b1e6a3cb459a2dd9a54
Adriana <adrianaherrera323@gmail.com>
2019-06-06 13:32:44 -0600

    Sensor, bluetooth y SMS listos

--- app/src/main/java/com/example/scomponents/JuegoFragment.java
+++ app/src/main/java/com/example/scomponents/JuegoFragment.java
@@ -1,20 +1,27 @@
 package com.example.scomponents;
 
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
@@ -22,6 +29,7 @@ import android.widget.TextView;
  */
 public class JuegoFragment extends Fragment implements SensorEventListener {
 
+    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 0;
     String[] palabras = {"Conducir","Hablar","Caminar","Reir","Sonreir","Pensar"};
     int x = 0;
     FrameLayout frameLayout;
@@ -29,6 +37,11 @@ public class JuegoFragment extends Fragment implements SensorEventListener {
     Sensor sensor;
     TextView mostrarPalabra, p1,p2;
     int c_jugador1 = 0, c_jugador2 = 0;
+    Button enviar;
+
+    String message;
+
+    String numeroTelefono = "0050689469996";
 
     public JuegoFragment() {
         // Required empty public constructor
@@ -49,9 +62,27 @@ public class JuegoFragment extends Fragment implements SensorEventListener {
         mostrarPalabra = view.findViewById(R.id.muestra);
         p1 = view.findViewById(R.id.p1);
         p2 = view.findViewById(R.id.p2);
+        enviar = (Button) view.findViewById(R.id.send_sms);
+
         mostrarPalabra.setText(palabras[0]);
         p1.setText("");
         p2.setText("");
+        enviar.setVisibility(View.INVISIBLE);
+
+        enviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    SmsManager smgr = SmsManager.getDefault();
+                    smgr.sendTextMessage(numeroTelefono,null,message,null,null);
+                    Toast.makeText(getContext(), "SMS enviado exitisamente!", Toast.LENGTH_SHORT).show();
+                }
+                catch (Exception e){
+                    Toast.makeText(getContext(), "Fallo al enviar SMS, intenta de nuevo.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
         return view;
     }
 
@@ -77,6 +108,10 @@ public class JuegoFragment extends Fragment implements SensorEventListener {
                 mostrarPalabra.setText("JUEGO TERMINADO");
                 p1.setText("Jugador 1: " + c_jugador1);
                 p2.setText("Jugador 2: " + c_jugador2);
+                enviar.setVisibility(View.VISIBLE);
+                message = "Eres el ganador del juego!\n" +
+                        "¡¡FELICIDADES!!\n" +
+                        "Tú puntaje fue de: " + c_jugador1 + " puntos.";
             }
         }else{
             if(x == 6){
@@ -88,9 +123,41 @@ public class JuegoFragment extends Fragment implements SensorEventListener {
         }
 
     }
+/*
+    protected void sendSMSMessage() {
+
+        if (ContextCompat.checkSelfPermission(getContext(),
+                Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                    Manifest.permission.SEND_SMS)) {
+            } else {
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{Manifest.permission.SEND_SMS},
+                        MY_PERMISSIONS_REQUEST_SEND_SMS);
+            }
+        }
+    }
 
     @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_SEND_SMS: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    SmsManager smsManager = SmsManager.getDefault();
+                    smsManager.sendTextMessage(numeroTelefono, null, message, null, null);
+                    Toast.makeText(getContext(), "SMS enviado.",
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getContext(),
+                            "Fallo al enviar SMS, intenta de nuevo!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+            }
+        }
+    }*/
 
-    }
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) { }
 }
--- app/src/main/java/com/example/scomponents/LoginFragment.java
+++ app/src/main/java/com/example/scomponents/LoginFragment.java
@@ -1,15 +1,24 @@
 package com.example.scomponents;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.Set;
 
 public class LoginFragment extends Fragment {
 
-    Button inicio;
+    Button inicio, bluetooth;
+    BluetoothAdapter AB;
+    Set<BluetoothDevice> pairedDEvices;
+    int flag = 0;
 
     public LoginFragment() {
         // Required empty public constructor
@@ -21,6 +30,24 @@ public class LoginFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_login, container, false);
         inicio = view.findViewById(R.id.inicio);
+        bluetooth = view.findViewById(R.id.bluetooth);
+
+        AB = BluetoothAdapter.getDefaultAdapter();
+
+        bluetooth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(flag == 0){
+                    on(v);
+                    visible(v);
+                    flag = 1;
+                }else{
+                    off(v);
+                    flag = 0;
+                }
+
+            }
+        });
 
         inicio.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,5 +57,26 @@ public class LoginFragment extends Fragment {
             }
         });
         return view;
+    }
+
+    public void on(View v){
+        if (!AB.isEnabled()) {
+            Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(turnOn, 0);
+            Toast.makeText(getContext(), "Bluetooth encendido!",Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getContext(), "Encendiendo..", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void off(View v){
+        AB.disable();
+        Toast.makeText(getContext(), "Bluetooth apagado!" ,Toast.LENGTH_LONG).show();
+    }
+
+
+    public  void visible(View v){
+        Intent getVisible = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        startActivityForResult(getVisible, 0);
     }
 }
