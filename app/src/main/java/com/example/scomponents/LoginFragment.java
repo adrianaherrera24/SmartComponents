fa20e8731d98fd27ab99a7af7ab40ea9f77df0ea
Leonardo <26692021+Lex417@users.noreply.github.com>
2019-06-07 00:55:34 -0600

    Camara Component

--- app/src/main/java/com/example/scomponents/LoginFragment.java
+++ app/src/main/java/com/example/scomponents/LoginFragment.java
@@ -1,25 +1,36 @@
 package com.example.scomponents;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
+import android.support.annotation.NonNull;
+import android.widget.ImageView;
+
 
 import java.util.Set;
 
+import javax.xml.transform.Result;
+
 public class LoginFragment extends Fragment {
 
-    Button inicio, bluetooth;
+    Button inicio, bluetooth, agregarUsuario;
+    ImageView imageView;
     BluetoothAdapter AB;
     Set<BluetoothDevice> pairedDEvices;
     int flag = 0;
-
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
     public LoginFragment() {
         // Required empty public constructor
     }
@@ -31,9 +42,10 @@ public class LoginFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_login, container, false);
         inicio = view.findViewById(R.id.inicio);
         bluetooth = view.findViewById(R.id.bluetooth);
-
+        agregarUsuario = view.findViewById(R.id.agregaJugador);
+        imageView= view.findViewById(R.id.imageView);
         AB = BluetoothAdapter.getDefaultAdapter();
-
+        final FragmentActivity activity = getActivity();
         bluetooth.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -48,7 +60,12 @@ public class LoginFragment extends Fragment {
 
             }
         });
-
+        agregarUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
         inicio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -78,5 +95,25 @@ public class LoginFragment extends Fragment {
     public  void visible(View v){
         Intent getVisible = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
         startActivityForResult(getVisible, 0);
+    }
+
+    private void dispatchTakePictureIntent() {
+
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == getActivity().RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+
+            imageView.setImageBitmap(imageBitmap);
+        }
     }
 }
